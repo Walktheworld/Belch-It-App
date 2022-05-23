4a84ca9ae82340a2e1745488582b7eb77c725091
Joe <Walktheworld@me.com>
2022-05-23 10:35:35 -0600

    initial resources

--- db/migrate/20220520205237_create_breweries.rb
+++ db/migrate/20220520205237_create_breweries.rb
@@ -1,6 +1,10 @@
 class CreateBreweries < ActiveRecord::Migration[6.1]
   def change
     create_table :breweries do |t|
+      t.string :name
+      t.string :address 
+      t.string :website
+      t.integer :phone
 
       t.timestamps
     end
